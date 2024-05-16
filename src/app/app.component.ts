26b81280a08bcf14c947d62bfb68ffc82d829576
martinizin <mar45pillacela@gmail.com>
2024-05-16 16:56:29 -0500

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-
 import { Component } from '@angular/core';
 import { SplashScreen } from '@capacitor/splash-screen'
 @Component({
@@ -7,17 +6,14 @@ import { SplashScreen } from '@capacitor/splash-screen'
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+splash: any;
   constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    /* To make sure we provide the fastest app loading experience
-       for our users, hide the splash screen automatically
-       when the app is ready to be used:
-
-        https://capacitor.ionicframework.com/docs/apis/splash-screen#hiding-the-splash-screen
-    */
+    
+  
     SplashScreen.hide();
   }
 }
